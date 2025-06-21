0832226275be03b754f0ff66909868ae6250eb82
NeoHospitalgit <digitalmkt@neohospital.com>
2025-06-21 10:57:19 +0530

    Update Hero.jsx

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -13,8 +13,8 @@ function Hero() {
   const sliderRef = useRef(null);
   const images = [
     "https://images.pexels.com/photos/4021775/pexels-photo-4021775.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    "/slider/2.jpg",
-    "/slider/1.jpg",
+    "/Assets/2.jpg",
+    "/Assets/1.jpg",
   ];
   const [currentSlide, setCurrentSlide] = useState(0);
 
