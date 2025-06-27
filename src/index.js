7eac0d28b19ec11a86c9f1d93c4755777e3c3e0f
NeoHospitalgit <digitalmkt@neohospital.com>
2025-06-27 16:58:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import { BrowserRouter as Router } from "react-router-dom";
-import './fontawesome';
+
 
 import App from "./App";
 import { ToastContainer } from "react-toastify";
