9867cda32418efee2c5ddae1ebf0a369868f43e1
NeoHospitalgit <digitalmkt@neohospital.com>
2025-07-03 10:24:30 +0530

    Update DemoDoctor.jsx

--- src/Pages/MeetOurDoctor/DemoDoctor.jsx
+++ src/Pages/MeetOurDoctor/DemoDoctor.jsx
@@ -88,7 +88,7 @@ function DemoDoctor() {
             </div>
           </div>
           <p className="about-description">
-            Neo Hospital places paramount importance on patient care, seamlessly
+            Neo Super Speciality Hospital places paramount importance on patient care, seamlessly
             merging cutting-edge medical advancements with heartfelt compassion.
             Our foundational principle is to craft an experience where every
             patient feels supported, efficient, and valued.
