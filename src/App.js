cf1e9e1296a2bc0f363c56bee9cac5f5162822bc
NeoHospitalgit <digitalmkt@neohospital.com>
2025-09-02 17:24:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import Service from "./Pages/Services/OurService.jsx";
 import ServiceDetails from "./Pages/Services/servicedetails.jsx";
 import BlogDetails from "./Pages/OurBlog/BlogDetails.jsx";
 import Sitemap from "./Pages/sitemap";
+import complaints from "./Pages/Complaints.jsx
 
 // manage page routes
 import Login from "./Pages/manage/Login.jsx";
