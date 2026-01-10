9b0259ede769c65e7217e150721558912a394930
unknown <you@example.com>
2026-01-10 17:18:30 +0530

    update css

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -336,7 +336,7 @@ function Footer() {
               <div className="row">
                 <div className="col-xl-6 col-lg-6 text-center text-lg-left">
                   <div className="copyright-text">
-                    <p>2025 All rights reserved © by neohospital.com</p>
+                    <p>2026 All rights reserved © by neohospital.com</p>
                   </div>
                 </div>
                 <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
