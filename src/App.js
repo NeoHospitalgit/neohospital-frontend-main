257457818966713ad986629698df2cf686abeb21
NeoHospitalgit <digitalmkt@neohospital.com>
2026-04-23 10:30:57 +0530

    Refactor routes for admin panel and authentication

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
 import { useAuth } from "./store/auth";
 import "./App.css";
 
-// Importing components
+// Components
 import Header from "./Pages/Header.jsx";
 import KeywordsPage from "./Pages/KeywordsPage.jsx";
 import Index from "./Pages/Home/Home.jsx";
@@ -23,12 +23,11 @@ import BlogDetails from "./Pages/OurBlog/BlogDetails.jsx";
 import Sitemap from "./Pages/sitemap";
 import CorporatePolicies from "./Pages/Corporate/CorporatePolicies.jsx";
 import BioMedicalReport from "./Pages/BioMedical/BioMedicalReport.jsx";
+import ManageBioReports from "./Pages/AdminPannel/ManageBioReports.jsx";
 
-// manage page routes
 import Login from "./Pages/manage/Login.jsx";
 import Logout from "./Pages/manage/Logout.jsx";
 
-// Importing other components
 import NotFound from "./Pages/NotFound.jsx";
 import DoctorDetails from "./Pages/MeetOurDoctor/DoctorDetails.jsx";
 import Contact from "./Pages/ContactUS/Contact.jsx";
@@ -37,35 +36,27 @@ import Career from "./Pages/CareerPage/Career.jsx";
 import Footer from "./Pages/Footer.jsx";
 import ScrollToTop from "./Pages/ScrollToTop.jsx";
 
-// admin all routes
+// Admin
 import Admin from "./Pages/AdminPannel/Admin.jsx";
-
 import AddHeader from "./Pages/AdminPannel/AddHeader.jsx";
 import ManageHeader from "./Pages/AdminPannel/ManageHeader.jsx";
-
 import AddTestimonials from "./Pages/AdminPannel/AddTestimonials.jsx";
 import ManageTestimonials from "./Pages/AdminPannel/ManageTestimonials.jsx";
-
 import AddSeoPages from "./Pages/AdminPannel/AddSeoPages.jsx";
 import ManageSeoPages from "./Pages/AdminPannel/ManageSeoPages.jsx";
-
 import Addblog from "./Pages/AdminPannel/Addblog.jsx";
 import Manageblog from "./Pages/AdminPannel/Manageblog.jsx";
-
 import AddSpecialitiy from "./Pages/AdminPannel/AddSpeciality.jsx";
 import ManageSpecialitiy from "./Pages/AdminPannel/ManageSpeciality.jsx";
-
 import AddDoctor from "./Pages/AdminPannel/AddDoctor.jsx";
 import ManageDoctor from "./Pages/AdminPannel/ManageDoctor.jsx";
-
 import ManageServices from "./Pages/AdminPannel/ManageService.jsx";
 import AddService from "./Pages/AdminPannel/AddService.jsx";
+import AddBioMedicalReport from "./Pages/AdminPannel/AddBioMedicalReport.jsx";
 import AddSeviceCate from "./Pages/AdminPannel/AddSeviceCate.jsx";
 import ManageServiceCategory from "./Pages/AdminPannel/ManageServiceCategory.jsx";
-
 import AddHomeDoctors from "./Pages/AdminPannel/AddHomeDoctors.jsx";
 import ManageHomeDoctors from "./Pages/AdminPannel/ManageHomeDoctors.jsx";
-
 import DetailsDepartment from "./Pages/OurDepartment/DetailsDepartment.jsx";
 
 const App = () => {
@@ -80,18 +71,22 @@ const App = () => {
       </div>
     );
   }
+
   return (
     <Router>
       <div className="App">
         {!isLoggedIn && <Header />}
         <ScrollToTop />
+
         <Routes>
           <Route path="*" element={<NotFound />} />
+
+          {/* ✅ FIXED HOME ROUTE */}
           <Route
             path="/"
-            // element={<Index />}
-            element={isLoggedIn ? <Navigate to="/login" /> : <Index />}
+            element={!isLoggedIn ? <Index /> : <Navigate to="/admin" />}
           />
+
           <Route path="/about" element={<About />} />
           <Route path="/specialities" element={<OurDepartment />} />
           <Route path="/doctors" element={<MeetOurDoctors />} />
@@ -100,12 +95,9 @@ const App = () => {
           <Route path="/services" element={<Service />} />
           <Route path="/corporate-policies" element={<CorporatePolicies />} />
           <Route path="/bio-medical-report" element={<BioMedicalReport />} />
-         <Route path="service/:service" element={<ServiceDetails />} />
-         <Route path="service/:service/:servicedetail" element={<ServiceDetails />} />
-          <Route
-            path="/international-patient"
-            element={<InternationPatient />}
-          />
+          <Route path="service/:service" element={<ServiceDetails />} />
+          <Route path="service/:service/:servicedetail" element={<ServiceDetails />} />
+          <Route path="/international-patient" element={<InternationPatient />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/career" element={<Career />} />
           <Route path="/blog/:blogs" element={<BlogDetails />} />
@@ -116,59 +108,44 @@ const App = () => {
           <Route path="/sitemap.xml" element={<Sitemap />} />
           <Route path="/doctor-details/:dr" element={<DoctorDetails />} />
 
-          {/* Admin Routes */}
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/add-header" element={<AddHeader />} />
-          <Route path="/add-header/:id" element={<AddHeader />} />
-          <Route path="/manage-header" element={<ManageHeader />} />
-          <Route path="/add-testimonials" element={<AddTestimonials />} />
-          <Route path="/add-testimonials/:id" element={<AddTestimonials />} />
-          <Route path="/manage-testimonials" element={<ManageTestimonials />} />
-          <Route path="/add-seopages" element={<AddSeoPages />} />
-          <Route path="/add-seopages/:id" element={<AddSeoPages />} />
-          <Route path="/manage-seopages" element={<ManageSeoPages />} />
-          <Route path="/add-blog" element={<Addblog />} />
-          <Route path="/add-blog/:id" element={<Addblog />} />
-          <Route path="/manage-blog" element={<Manageblog />} />
-          <Route path="/add-specialities" element={<AddSpecialitiy />} />
-          <Route path="/add-specialities/:id" element={<AddSpecialitiy />} />
-          <Route path="/manage-specialities" element={<ManageSpecialitiy />} />
-          <Route path="/add-doctors" element={<AddDoctor />} />
-          <Route path="/add-doctors/:id" element={<AddDoctor />} />
-          <Route path="/manage-doctors" element={<ManageDoctor />} />
-          <Route path="/add-home-doctors" element={<AddHomeDoctors />} />
-          <Route path="/add-home-doctors/:id" element={<AddHomeDoctors />} />
-          <Route path="/manage-home-doctors" element={<ManageHomeDoctors />} />
-          <Route path="/manage-service" element={<ManageServices />} />
-          <Route path="/add-service" element={<AddService />} />
-          <Route path="/add-service/:id" element={<AddService />} />
-          <Route path="/add-service-category" element={<AddSeviceCate />} />
-          <Route path="/add-service-category/:id" element={<AddSeviceCate />} />
-          <Route
-            path="/manage-service-category"
-            element={<ManageServiceCategory />}
-          />
+          {/* ✅ PROTECTED ADMIN ROUTES */}
+          <Route path="/admin" element={isLoggedIn ? <Admin /> : <Navigate to="/login" />} />
+          <Route path="/add-header" element={isLoggedIn ? <AddHeader /> : <Navigate to="/login" />} />
+          <Route path="/add-header/:id" element={isLoggedIn ? <AddHeader /> : <Navigate to="/login" />} />
+          <Route path="/manage-header" element={isLoggedIn ? <ManageHeader /> : <Navigate to="/login" />} />
+          <Route path="/add-testimonials" element={isLoggedIn ? <AddTestimonials /> : <Navigate to="/login" />} />
+          <Route path="/add-testimonials/:id" element={isLoggedIn ? <AddTestimonials /> : <Navigate to="/login" />} />
+          <Route path="/manage-testimonials" element={isLoggedIn ? <ManageTestimonials /> : <Navigate to="/login" />} />
+          <Route path="/add-seopages" element={isLoggedIn ? <AddSeoPages /> : <Navigate to="/login" />} />
+          <Route path="/add-seopages/:id" element={isLoggedIn ? <AddSeoPages /> : <Navigate to="/login" />} />
+          <Route path="/manage-seopages" element={isLoggedIn ? <ManageSeoPages /> : <Navigate to="/login" />} />
+          <Route path="/add-blog" element={isLoggedIn ? <Addblog /> : <Navigate to="/login" />} />
+          <Route path="/add-blog/:id" element={isLoggedIn ? <Addblog /> : <Navigate to="/login" />} />
+          <Route path="/manage-blog" element={isLoggedIn ? <Manageblog /> : <Navigate to="/login" />} />
+          <Route path="/add-specialities" element={isLoggedIn ? <AddSpecialitiy /> : <Navigate to="/login" />} />
+          <Route path="/add-specialities/:id" element={isLoggedIn ? <AddSpecialitiy /> : <Navigate to="/login" />} />
+          <Route path="/manage-specialities" element={isLoggedIn ? <ManageSpecialitiy /> : <Navigate to="/login" />} />
+          <Route path="/add-doctors" element={isLoggedIn ? <AddDoctor /> : <Navigate to="/login" />} />
+          <Route path="/add-doctors/:id" element={isLoggedIn ? <AddDoctor /> : <Navigate to="/login" />} />
+          <Route path="/manage-doctors" element={isLoggedIn ? <ManageDoctor /> : <Navigate to="/login" />} />
+          <Route path="/add-home-doctors" element={isLoggedIn ? <AddHomeDoctors /> : <Navigate to="/login" />} />
+          <Route path="/add-home-doctors/:id" element={isLoggedIn ? <AddHomeDoctors /> : <Navigate to="/login" />} />
+          <Route path="/manage-home-doctors" element={isLoggedIn ? <ManageHomeDoctors /> : <Navigate to="/login" />} />
+          <Route path="/manage-service" element={isLoggedIn ? <ManageServices /> : <Navigate to="/login" />} />
+          <Route path="/add-service" element={isLoggedIn ? <AddService /> : <Navigate to="/login" />} />
+          <Route path="/add-service/:id" element={isLoggedIn ? <AddService /> : <Navigate to="/login" />} />
+          <Route path="/add-service-category" element={isLoggedIn ? <AddSeviceCate /> : <Navigate to="/login" />} />
+          <Route path="/add-service-category/:id" element={isLoggedIn ? <AddSeviceCate /> : <Navigate to="/login" />} />
+          <Route path="/manage-service-category" element={isLoggedIn ? <ManageServiceCategory /> : <Navigate to="/login" />} />
+          <Route path="/add-bio-medical-report" element={isLoggedIn ? <AddBioMedicalReport /> : <Navigate to="/login" />} />
+          <Route path="/add-bio-medical-report/:id" element={isLoggedIn ? <AddBioMedicalReport /> : <Navigate to="/login" />} />
+          <Route path="/manage-medical-report" element={isLoggedIn ? <ManageBioReports /> : <Navigate to="/login" />} />
         </Routes>
+
         {!isLoggedIn && <Footer />}
       </div>
     </Router>
   );
 };
-
-{
-  /* {userRole === "seo" || userRole === "dev" ? ( */
-}
-{
-  /* <ManageServiceCategory /> */
-}
-{
-  /* ) : ( */
-}
-{
-  /*     <Navigate to="/admin" /> */
-}
-{
-  /*    )} */
-}
 
 export default App;
