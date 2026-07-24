fdb03b2b949521f1e7f691f25e5bf6306b013615
NeoHospitalgit <digitalmkt@neohospital.com>
2026-07-24 13:16:03 +0530

    Update DoctorsSection.jsx

--- src/Pages/KeywordPage/DoctorsSection.jsx
+++ src/Pages/KeywordPage/DoctorsSection.jsx
@@ -1,98 +1,103 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaCalendarCheck, FaUserMd } from "react-icons/fa";
-import "./Keyword.css";
+import { FaUserMd } from "react-icons/fa";
 import parse from "html-react-parser";
+import AppointmentModal from "../AppointmentModal/AppointmentModal";
+import "./Keyword.css";
 
 function DoctorsSection({
-  doctors,
+  doctors = [],
   API,
   teamTitle,
   teamContent,
+  doctorname,
 }) {
   return (
     <section className="doctor-section">
       <div className="container">
 
+        {/* Section Heading */}
         <div className="section-heading">
-
           <span className="section-tag">
-            Our Teams
+            Our Team
           </span>
 
           <h2>
-            {teamTitle}
+            {teamTitle || `Meet Our ${doctorname || "Specialists"}`}
           </h2>
 
-        
-          {teamContent && parse(teamContent)}
-       
-
+          {teamContent && (
+            <div className="section-content">
+              {parse(teamContent)}
+            </div>
+          )}
         </div>
 
+        {/* Doctor Cards */}
         <div className="row">
+          {doctors && doctors.length > 0 ? (
+            doctors.map((doctor) => (
+              <div
+                className="col-lg-4 col-md-6 mb-4"
+                key={doctor._id}
+              >
+                <div className="doctor-card">
 
-          {doctors.map((doctor) => (
-
-            <div
-              className="col-lg-4 col-md-6 mb-4"
-              key={doctor._id}
-            >
-
-              <div className="doctor-card">
-
-                <div className="doctor-image">
-
-                  <img
-                    src={`${API}/uploads/doctors/${doctor.drImage}`}
-                    alt={doctor.drTitle}
-                  />
-
-                </div>
-
-                <div className="doctor-body">
-
-                  <span className="doctor-speciality">
-                    <FaUserMd />
-                    Specialist
-                  </span>
-
-                  <h3>
-                    {doctor.drTitle}
-                  </h3>
-
-                  <p className="qualification">
-                    {doctor.drQualification}
-                  </p>
-
-                
-                  <div className="doctor-buttons">
-
-                    <a
-                      href="/appointment"
-                      className="book-btn"
-                    >
-                    
-                      Book Appointment
-                    </a>
-
-                    <Link
-                      to={`/doctor-details/${doctor.drSlug || doctor._id}`}
-                      className="profile-btn"
-                    >
-                      View Profile
-                    </Link>
-
+                  <div className="doctor-image">
+                    <img
+                      src={
+                        doctor.drImage
+                          ? `${API}/uploads/doctors/${doctor.drImage}`
+                          : "/images/default-doctor.png"
+                      }
+                      alt={doctor.drTitle}
+                      loading="lazy"
+                    />
                   </div>
 
+                  <div className="doctor-body">
+
+                    <span className="doctor-speciality">
+                      <FaUserMd />
+                      Specialist
+                    </span>
+
+                    <h3>{doctor.drTitle}</h3>
+
+                    {doctor.drQualification && (
+                      <p className="qualification">
+                        {doctor.drQualification}
+                      </p>
+                    )}
+
+                    <div className="doctor-buttons">
+                      {/* <AppointmentModal
+                        buttonText="Book Appointment"
+                        doctorname={doctor.drTitle}
+                      /> */}
+
+                      <Link
+                        to={`/doctor-details/${doctor.drSlug || doctor._id}`}
+                        className="profile-btn"
+                      >
+                        View Profile
+                      </Link>
+                    </div>
+
+                  </div>
                 </div>
-
               </div>
-
+            ))
+          ) : (
+            <div className="col-12">
+              <div className="text-center py-5">
+                <h4>No Doctors Available</h4>
+                <p>
+                  Our specialist information will be updated soon.
+                </p>
+              </div>
             </div>
-
-          ))}
-
+          )}
         </div>
 
       </div>
