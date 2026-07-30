4e66a2ad21577544671eda4f8fdf97ffec1354f5
NeoHospitalgit <digitalmkt@neohospital.com>
2026-07-30 12:46:36 +0530

    Add files via upload

---
+++ src/Pages/Procedures/CTA.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { FaPhoneAlt, FaCalendarCheck } from "react-icons/fa";
+import AppointmentModal from "../AppointmentModal/AppointmentModal";
+import "./ProceduresPage.css";
+
+function CTA({ pageData }) {
+    console.log('tert',pageData);
+  const [showAppointment, setShowAppointment] = useState(false);
+
+  return (
+    <>
+      <section className="procedure-cta">
+
+        <div className="procedure-cta-content">
+
+          <span>NEO Hospital</span>
+
+          <h2>
+            {pageData?.cat_title || "Looking for the Right Treatment?"}
+          </h2>
+
+          <p>
+            {pageData?.cat_content ||
+              "Connect with our experienced specialists and get the best treatment with personalized care at NEO Hospital."}
+          </p>
+
+          <div className="procedure-cta-buttons">
+
+           <AppointmentModal
+        show={showAppointment}
+        onClose={() => setShowAppointment(false)}
+        doctorname=""
+      />
+            <a
+              href="tel:01204880088"
+              className="cta-btn btn-outlines"
+            >
+              <FaPhoneAlt />
+              Call Now
+            </a>
+
+          </div>
+
+        </div>
+
+      </section>
+
+      
+    </>
+  );
+}
+
+export default CTA;
