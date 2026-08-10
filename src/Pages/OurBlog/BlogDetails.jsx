fc44f2b390e54f73d78c016288772f7189886a54
NeoHospitalgit <digitalmkt@neohospital.com>
2026-08-10 10:57:27 +0530

    Add margin to author info section

--- src/Pages/OurBlog/BlogDetails.jsx
+++ src/Pages/OurBlog/BlogDetails.jsx
@@ -193,7 +193,7 @@ const BlogDetails = () => {
                 alt={blog.blog_title}
                 className="img-fluid"
               />
-               <div className="author-info">
+               <div className="author-info mt-5">
              <h6 className="author-name">Author: {blog.blog_auther}</h6>
             </div>
               <h1 className="blogtitle">{blog.blog_title}</h1>
