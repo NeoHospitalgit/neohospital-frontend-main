e88efe6c0adaa0b10fbca578c3e72e022c12ac38
NeoHospitalgit <digitalmkt@neohospital.com>
2026-08-12 11:57:21 +0530

    Add files via upload

---
+++ src/Pages/ContactUS/Career.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+// import Corevalue from "../About/Corevalue";
+import "./Career.css";
+import CareerForm from "./CareerForm";
+// import { Contactseo } from "../SeoContent";
+// import { Helmet } from "react-helmet";
+// import parse from "html-react-parser";
+
+function Contact() {
+  return (
+    <>
+      {/* <Helmet>{parse(Contactseo.meetafamily)}</Helmet> */}
+      {/* <Corevalue /> */}
+      <CareerForm />
+    </>
+  );
+}
+
+export default Contact;
---
+++ src/Pages/ContactUS/Careerbanner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Careerban from "../../Assets/Banners/contactus.jpg";
+function Careerbanner() {
+  return (
+    <>
+      <section id="contact">
+        <img src={Careerban} alt="" srcset="" className="banner-img" />
+      </section>
+    </>
+  );
+}
+
+export default Careerbanner;
